04807c4ffc8b66cb548276337996a9b0fc4465d1
liulebin <liulebinn@163.com>
2021-04-12 23:24:50 +0800

    nacos作为配置中心管理配置

--- gulimall-coupon/src/main/java/com/llb/mall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/llb/mall/coupon/controller/CouponController.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,10 +27,21 @@ import com.llb.common.utils.R;
  * @date 2021-04-10 22:19:26
  */
 @RestController
+@RefreshScope
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test() {
+        return new R().put("name", name).put("age", age);
+    }
 
     /**
      * 列表
