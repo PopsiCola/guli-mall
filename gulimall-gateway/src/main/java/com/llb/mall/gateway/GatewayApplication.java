fa82bfb8b848f3856dba725f96792f440fe13746
liulebin <liulebinn@163.com>
2021-04-13 23:06:06 +0800

    搭建gateway模块

---
+++ gulimall-gateway/src/main/java/com/llb/mall/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.llb.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author liulebin
+ * @Date 2021/4/13 22:44
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
