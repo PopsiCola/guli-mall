f241958f12e4dace58fa61dc8bcbc058c3208cba
liulebin <liulebinn@163.com>
2021-05-04 22:49:16 +0800

    用户系统-> 会员等级信息维护

--- gulimall-member/src/main/java/com/llb/mall/member/MemberApplication.java
+++ gulimall-member/src/main/java/com/llb/mall/member/MemberApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Author liulebin
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @MapperScan("com.llb.mall.member.dao")
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.llb.mall.member.feign")
 public class MemberApplication {
 
     public static void main(String[] args) {
