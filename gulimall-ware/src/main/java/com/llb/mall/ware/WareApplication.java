c1cb0044936a0072c1f434a967f6f355f044715d
liulebin <liulebinn@163.com>
2021-05-08 21:25:35 +0800

    库存系统-> 仓库信息维护

--- gulimall-ware/src/main/java/com/llb/mall/ware/WareApplication.java
+++ gulimall-ware/src/main/java/com/llb/mall/ware/WareApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Author liulebin
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @MapperScan("com.llb.mall.ware.dao")
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class WareApplication {
 
     public static void main(String[] args) {
