211034e717e28c28567fceaad23b2a04e3fc8564
liulebin <liulebinn@163.com>
2021-05-14 22:26:29 +0800

    Elasticsearch创建索引

--- gulimall-search/src/main/java/com/llb/mall/search/config/ElasticSearchConfig.java
+++ gulimall-search/src/main/java/com/llb/mall/search/config/ElasticSearchConfig.java
@@ -1,9 +1,7 @@
 package com.llb.mall.search.config;
 
 import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
-import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +12,16 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class ElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        /*builder.addHeader("Authorization", "Bearer " + TOKEN);
+        builder.setHttpAsyncResponseConsumerFactory(
+                new HttpAsyncResponseConsumerFactory
+                        .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));*/
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- gulimall-search/src/test/java/com/llb/mall/search/TestSearch.java
+++ gulimall-search/src/test/java/com/llb/mall/search/TestSearch.java
@@ -1,11 +1,19 @@
 package com.llb.mall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.llb.mall.search.config.ElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 /**
  * @Author liulebin
@@ -17,6 +25,36 @@ public class TestSearch {
 
     @Autowired
     private RestHighLevelClient client;
+
+    /**
+     * 测试存储数据到es，
+     * 更新也可以
+     */
+    @Test
+    public void testIndex() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+
+        User user = new User();
+        user.setUserName("张三");
+        user.setGender("男");
+        user.setAge(23);
+
+        String json = JSON.toJSONString(user);
+        indexRequest.source(json, XContentType.JSON);
+
+        // 执行操作
+        IndexResponse index = client.index(indexRequest, ElasticSearchConfig.COMMON_OPTIONS);
+
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
+    }
 
     @Test
     public void test() {
